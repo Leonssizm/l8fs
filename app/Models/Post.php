9a326949276ee4bedb1628c5205b428fb16800fe
Leonssizm <levan.kereselidze@redberry.ge>
2023-03-22 23:30:54 +0400

    feat:post page metadata

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,29 +2,50 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+	public $title;
+
+	public $excerpt;
+
+	public $date;
+
+	public $body;
+
+	public $slug;
+
+	public function __construct($title, $excerpt, $date, $body, $slug)
+	{
+		$this->title = $title;
+		$this->excerpt = $excerpt;
+		$this->date = $date;
+		$this->slug = $slug;
+		$this->body = $body;
+	}
+
 	public static function all()
 	{
-		$files = File::files(resource_path('posts/'));
-
-		return array_map(fn ($file) => $file->getContents(), $files);
+		return cache()->rememberForever('posts.all', function () {
+			return collect(File::files(resource_path('posts')))->map(fn ($file) => YamlFrontMatter::parseFile($file))
+			->map(function ($document) {
+				return new Post(
+					$document->title,
+					$document->excerpt,
+					$document->date,
+					$document->body(),
+					$document->slug,
+				);
+			})->sortByDesc('date');
+		});
 	}
 
 	public static function find($slug)
 	{
-		if (!file_exists($path = resource_path("/posts/{$slug}.html")))
-		{
-			throw new ModelNotFoundException();
-		}
+		$posts = static::all();
 
-		return cache()->remember(
-			"posts.{$slug}",
-			10,
-			fn () => file_get_contents($path)
-		);
+		return $posts->firstWhere('slug', $slug);
 	}
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,21 +5,16 @@
     
     <?php foreach($posts as $post) : ?>
         <article>
-            <?= $post; ?>
+            <h1>
+            <a href="/posts/<?= $post->slug; ?>">
+                <?= $post->title; ?>
+            </a>
+            </h1>
+            
+            <div>
+                <?= $post->excerpt; ?>
+            </div>
         </article>
     <?php endforeach; ?>
-    <!-- <article>
-        <h1> <a href="/posts/my-first-post">My First Post</a></h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque aspernatur, sint voluptatum necessitatibus quis possimus nobis quo aliquid? Nulla rem praesentium natus quisquam repudiandae, harum sapiente facere ratione non omnis.</p>
-    </article>
 
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque aspernatur, sint voluptatum necessitatibus quis possimus nobis quo aliquid? Nulla rem praesentium natus quisquam repudiandae, harum sapiente facere ratione non omnis.</p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque aspernatur, sint voluptatum necessitatibus quis possimus nobis quo aliquid? Nulla rem praesentium natus quisquam repudiandae, harum sapiente facere ratione non omnis.</p>
-    </article> -->
 </body>
