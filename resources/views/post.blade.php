9f1135fb90e236ec857066447f680c389610cef2
Leonssizm <levan.kereselidze@redberry.ge>
2023-03-22 20:44:43 +0400

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,10 @@
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+       <?=$post; ?>
+    </article>
+
+<a href="/">Go Back</a>
+
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+        <h1> <a href="/post">My First Post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque aspernatur, sint voluptatum necessitatibus quis possimus nobis quo aliquid? Nulla rem praesentium natus quisquam repudiandae, harum sapiente facere ratione non omnis.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque aspernatur, sint voluptatum necessitatibus quis possimus nobis quo aliquid? Nulla rem praesentium natus quisquam repudiandae, harum sapiente facere ratione non omnis.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque aspernatur, sint voluptatum necessitatibus quis possimus nobis quo aliquid? Nulla rem praesentium natus quisquam repudiandae, harum sapiente facere ratione non omnis.</p>
+    </article>
+</body>
