1d584e73d5aa0c33c62d7a4936a31a9c41e65cb5
Leonssizm <levan.kereselidze@redberry.ge>
2023-03-24 09:37:14 +0400

    feat: fake post data factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
 use App\Models\Post;
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,49 +13,6 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run(): void
 	{
-		User::truncate();
-		Post::truncate();
-		Category::truncate();
-
-		$user = User::factory()->create();
-
-		$personal = Category::create([
-			'name' => 'Personal',
-			'slug' => 'personal',
-		]);
-
-		$work = Category::create([
-			'name' => 'Work',
-			'slug' => 'work',
-		]);
-		$home = Category::create([
-			'name' => 'Home',
-			'slug' => 'home',
-		]);
-
-		Post::create([
-			'user_id'     => $user->id,
-			'category_id' => $personal->id,
-			'title'       => 'MY FIRST POST',
-			'slug'        => 'my-first-post',
-			'excerpt'     => 'first post excerpt',
-			'body'        => 'asdasdadslkjaflkjqljelhjqefjbfds,ansbjsahdbjqhwdkjqwhaskjdnalksdnasd',
-		]);
-		Post::create([
-			'user_id'     => $user->id,
-			'category_id' => $work->id,
-			'title'       => 'MY Second POST',
-			'slug'        => 'my-second-post',
-			'excerpt'     => 'second post excerpt',
-			'body'        => 'asdasdadslkjaflkjqljelhjqefjbfds,ansbjsahdbjqhwdkjqwhaskjdnalksdnasd',
-		]);
-		Post::create([
-			'user_id'     => $user->id,
-			'category_id' => $home->id,
-			'title'       => 'MY second POST',
-			'slug'        => 'my-third-post',
-			'excerpt'     => 'second post excerpt',
-			'body'        => 'asdasdadslkjaflkjqljelhjqefjbfds,ansbjsahdbjqhwdkjqwhaskjdnalksdnasd',
-		]);
+		Post::factory(5)->create();
 	}
 }
