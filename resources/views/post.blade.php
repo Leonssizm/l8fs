ce3d581d5f3512671f1967ad9e0e5ecb0df81559
Leonssizm <levan.kereselidze@redberry.ge>
2023-03-25 14:30:53 +0400

    feat:query search filter function

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -41,24 +41,13 @@
                         </div>
     
                         <h1 class="font-bold text-3xl lg:text-4xl mb-10">
-                            This is a big title and it will look great on two or even three
-                            lines. Wooohoo!
+                            {{$post->title}}
                         </h1>
     
                         <div class="space-y-4 lg:text-lg leading-loose">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                laboris nisi ut aliquip ex ea commodo consequat.</p>
-    
-                            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                pariatur.</p>
-    
-                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                laudantium,
-                                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
-                                vitae
-                                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-                                fugit.</p>
+                            <p>
+                                {{$post->body}}
+                            </p>
     
                             <h2 class="font-bold text-lg">Sed quia consequuntur</h2>
     
