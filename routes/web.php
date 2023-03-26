2105ccd8c54adb465bf58496e2a68386ba967cdb
Leonssizm <levan.kereselidze@redberry.ge>
2023-03-26 16:48:12 +0400

    feat: email newsletter subscription

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,8 @@ Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 // Comments
 Route::post('posts/{post:id}/comments', [PostCommentsController::class, 'store']);
 
-Route::get('ping', function () {
+Route::post('newsletter', function () {
+	request()->validate(['email' => 'required|email']);
 	$mailchimp = new \MailchimpMarketing\ApiClient();
 
 	$mailchimp->setConfig([
@@ -57,10 +58,19 @@ Route::get('ping', function () {
 		'server' => 'us18',
 	]);
 
-	$response = $mailchimp->lists->addListMember('7fe5120b39', [
-		'email_address' => 'Lindsey.White93@hotmail.com',
-		'status'        => 'pending',
-	]);
+	try
+	{
+		$response = $mailchimp->lists->addListMember('7fe5120b39', [
+			'email_address' => request('email'),
+			'status'        => 'subscribed',
+		]);
+	}
+	catch (\Exception $e)
+	{
+		throw \Illuminate\Validation\ValidationException::withMessages([
+			'email' => 'This email could not be added to our newsletter list',
+		]);
+	}
 
-	ddd($response);
+		return redirect('/')->with('success', 'you are now signed up for our newsletter');
 });
